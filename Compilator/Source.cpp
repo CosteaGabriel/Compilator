9372e64fd28441578f67ac41e919eb7676aa12c5
CosteaGabriel <costea.gabriel19@gmail.com>
2017-04-09 22:40:37 +0300

    ALL

--- Compilator/Source.cpp
+++ Compilator/Source.cpp
@@ -1,36 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <string.h>
 
 #define SAFEALLOC(var, Type) if((var=(Type*)malloc(sizeof(Type))) == NULL) err("not enough memory");
 
 enum{
 	ID,
 	CT_INT, CT_REAL, CT_CHAR, CT_STRING,
+	BREAK, CHAR, INT, DOUBLE, ELSE, FOR , IF, RETURN, STRUCT, VOID, WHILE,
 	COMMA, SEMICOLON, LPAR, RPAR, LBRACKET, RBRACKET, LACC, RACC,
 	ADD, SUB, MUL, DIV, DOT, AND, OR, NOT, ASSIGN, EQUAL, NOTEQ, LESS, LESSEQ, GREATER, GREATEREQ,
 	SPACE, LINECOMMENT, COMMENT,
 	END
 }; // codurile AL
 
-typedef struct _TOKEN
+
+char *names[] = { "ID",
+"CT_INT", "CT_REAL", "CT_CHAR", "CT_STRING",
+"BREAK", "CHAR", "INT", "DOUBLE", "ELSE", "FOR", "IF", "RETURN", "STRUCT", "VOID", "WHILE",
+"COMMA", "SEMICOLON", "LPAR", "RPAR", "LBRACKET", "RBRACKET", "LACC", "RACC",
+"ADD", "SUB", "MUL", "DIV", "DOT", "AND", "OR", "NOT", "ASSIGN", "EQUAL", "NOTEQ", "LESS", "LESSEQ", "GREATER", "GREATEREQ",
+"SPACE", "LINECOMMENT", "COMMENT",
+"END" };
+
+typedef struct _Token
 {
 	int code;				// codul (numele)
-
 	union
 	{
 		char *text;			// folosit pentru ID, CT_STRING (alocat dinamic)
 		long int i;			// folosit pentru CT_INT, CT_CHAR
 		double r;			// folosit pentru CT_REAL
 	};
-
 	int line;				// linia din fisierul de intrare
 	struct _Token *next;	// inlantuire la urmatorul AL
 }Token;
 
 char input[50001];
-int line;
+int line = 1;
 
 char *pCrtCh;
+
+char *int_char, *real;
 
 Token *tokens = NULL;
 Token *lastToken = NULL;
@@ -50,7 +63,7 @@ void tkerr(const Token *tk, const char *fmt, ...)
 {
 	va_list va;
 	va_start(va, fmt);
-	fprintf(stderr, "error in line %d", tk->line);
+	fprintf(stderr, "error in line %d ", tk->line);
 	vfprintf(stderr, fmt, va);
 	fputc('\n', stderr);
 	va_end(va);
@@ -75,16 +88,40 @@ Token *addTk(int code)
 	{
 		tokens = tk;
 	}
-
+	lastToken = tk;
 	return tk;
+}
+
+char *createString(const char *pStartCh, char *pCrtCh)
+{
+	int size = pCrtCh - pStartCh;
+	int idx= 0;
+
+	char *text;
+
+	text = (char*)malloc(sizeof(char)*size + 1);
+	text[size]='\0';
+
+		while (idx < size)
+		{
+			text[idx] = *pStartCh;
+			pStartCh++;
+			idx++;
+		}
+
+	return text;
 }
 
 int getNextToken()
 {
 	int			state = 0, nCh;
-	char		*ch;
+	char ch;
+
+	bool comment = false, hex = false;
 	const char	*pStartCh;
 	Token		*tk;
+
+	SAFEALLOC(tk, Token);
 
 	while (1)
 	{
@@ -93,24 +130,583 @@ int getNextToken()
 		switch (state)
 		{
 		case 0:
-			if (isalpha(ch) || ch == '_')
+			if (ch >= '1' && ch <= '9')
 			{
 				pStartCh = pCrtCh;
 				pCrtCh++;
-				//state = ;
+				state = 1;
 			}
-			else tkerr(addTk(END), "caracter invalid");
+			else if (ch == '0')
+			{
+				pStartCh = pCrtCh;
+				pCrtCh++;
+				state = 2;
+			}
+			else if (ch == '\'')
+			{
+				pStartCh = pCrtCh;
+				pCrtCh++;
+				state = 12;
+			}
+			else if (ch == '"')
+			{
+				pStartCh = pCrtCh;
+				pCrtCh++;
+				state = 16;
+			}
+			else if (ch == '/')
+			{
+				pStartCh = pCrtCh;
+				pCrtCh++;
+				state = 17;
+			}
+			else if (ch == ',')
+			{
+				pCrtCh++;
+				state = 35;
+			}
+			else if (ch == ';')
+			{
+				pCrtCh++;
+				state = 37;
+			}
+			else if (ch == ' ' || ch == '\r' || ch == '\t'){
+				pCrtCh++;										// consuma caracterul si ramane in starea 0
+			}
+			else if (isalpha(ch) || ch == '_')
+			{
+				pStartCh = pCrtCh;
+				pCrtCh++;
+				state = 22;
+			}
+			else if (ch == '(')
+			{
+				pCrtCh++;
+				state = 38;
+			}
+			else if (ch == ')')
+			{
+				pCrtCh++;
+				state = 39;
+			}
+			else if (ch == ']')
+			{
+				pCrtCh++;
+				state = 40;
+			}
+			else if (ch == ']')
+			{
+				pCrtCh++;
+				state = 41;
+			}
+			else if (ch == '!')
+			{
+				pCrtCh++;
+				state = 23;
+			}
+			else if (ch == '<')
+			{
+				pCrtCh++;
+				state = 24;
+			}
+			else if (ch == '>')
+			{
+				pCrtCh++;
+				state = 25;
+			}
+			else if (ch == '{')
+			{
+				pCrtCh++;
+				state = 48;
+			}
+			else if (ch == '}')
+			{
+				pCrtCh++;
+				state = 49;
+			}
+			else if (ch == '+')
+			{
+				pCrtCh++;
+				state = 50;
+			}
+			else if (ch == '-')
+			{
+				pCrtCh++;
+				state = 51;
+			}
+			else if (ch == '*')
+			{
+				pCrtCh++;
+				state = 52;
+			}
+			else if (ch == '&')
+			{
+				pCrtCh++;
+				state = 27;
+			}
+			else if (ch == '|')
+			{
+				pCrtCh++;
+				state = 28;
+			}
+			else if (ch == '.')
+			{
+				pCrtCh++;
+				state = 55;
+			}
+			else if (ch == '=')
+			{
+				pCrtCh++;
+				state = 26;
+			}
+			else if (ch == '\n'){ // tratat separat pentru a actualiza linia curenta
+				line++;
+				pCrtCh++;
+			}
+			else if (ch == '\0'){ // sfarsit de sir
+				state = 59;
+			}
+			else tkerr(addTk(END), "caracter invalid 0\n");
 			break;
-
+		case 1:
+			if (isdigit(ch))
+				pCrtCh++;
+			else if (ch == 'e' || ch == 'E')
+			{
+				pCrtCh++;
+				state = 9;
+			}
+			else if (ch == 'e' || ch == 'E')
+			{
+				pCrtCh++;
+				state == 9;
+			}
+			else if (ch == '.')
+			{
+				pCrtCh++;
+				state = 7;
+			}
+			else if (!isdigit(ch))
+				state = 29;
+			else tkerr(addTk(END), "caracter invalid 1");
+			break;
+		case 2:
+			if (ch == 'x')
+			{
+				pCrtCh++;
+				hex = true;
+				state = 4;
+			}
+			else
+				state = 3;
+			break;
+		case 3:
+			if (ch >= '0' && ch <= '7')
+			{
+				pCrtCh++;
+			}
+			else if (ch == 'e' || ch == 'E')
+			{
+				pCrtCh++;
+				state == 9;
+			}
+			else if (ch > '7')
+			{
+				pCrtCh++;
+				state = 6;
+			}
+			else
+				state = 29;
+			break;
+		case 4:
+			if ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F'))
+			{
+				pCrtCh++;
+				state = 5;
+			}
+			else tkerr(addTk(END), "caracter invalid 4");
+			break;
+		case 5:
+			if ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F'))
+			{
+				pCrtCh++;
+			}
+			else
+				state = 29;
+			break;
+		case 6:
+			if (ch >= '0' && ch <= '9')
+			{
+				pCrtCh++;
+			}
+			else if (ch == '.')
+			{
+				pCrtCh++;
+				state = 7;
+			}
+			else if (ch == 'e' || ch == 'E')
+			{
+				pCrtCh++;
+				state = 9;
+			}
+			else tkerr(addTk(END), "caracter invalid 6\n");
+			break;
+		case 7:
+			if (ch >= '0' && ch <= '9')
+			{
+				pCrtCh++;
+				state = 8;
+			}
+			else tkerr(addTk(END), "caracter invalid 7\n");
+			break;
+		case 8:
+			if (ch >= '0' && ch <= '9')
+			{
+				pCrtCh++;
+			}
+			else if (ch == 'e' || ch == 'E')
+			{
+				pCrtCh++;
+				state = 9;
+			}
+			else 
+				state=30;
+			break;
+		case 9:
+			if (ch == '-' || ch == '+')
+			{
+				pCrtCh++;
+				state = 10;
+			}
+			else
+				state = 10;
+			break;
+		case 10:
+			if (ch >= '0' && ch <= '9')
+			{
+				pCrtCh++;
+				state = 11;
+			}
+			else tkerr(addTk(END), "caracter invalid 10\n");
+			break;
+		case 11:
+			if (ch >= '0' && ch <= '9')
+			{
+				pCrtCh++;
+			}
+			else
+				state = 30;
+			break;
+		case 12:
+			if (ch != '\'' && ch != '\\')
+			{
+				pCrtCh++;
+				state = 13;
+			}
+			else if (ch == '\\')
+			{
+				pCrtCh++;
+				state = 14;
+			}
+			else 
+				tkerr(addTk(END), "caracter invalid 12\n");
+			break;
+		case 13:
+			if (ch == '\'')
+			{
+				pCrtCh++;
+				state = 32;
+			}
+			else
+				state = 16;
+			break;
+		case 14:
+			if (ch == 'a' || ch == 'b' || ch == 'f' || ch == 'n' || ch == 'r' || ch == 't' || ch == 't' || ch == 'v' || ch == '?' || ch == '\'' || ch == '"' | ch == '\\' || ch == '0')
+			{
+				pCrtCh++;
+				state = 15;
+			}
+			else 
+				tkerr(addTk(END), "caracter invalid 14\n");
+			break;
+		case 15:
+			if (ch == '\'')
+			{
+				pCrtCh++;
+				state = 32;
+			}
+			else if (ch == '"')
+			{
+				pCrtCh++;
+				state = 33;
+			}
+			else
+				state = 16;
+			break;
+		case 16:
+			if (ch != '\'' && ch != '\\')
+				pCrtCh++;
+			else if (ch == '\\')
+			{
+				pCrtCh++;
+				state = 14;
+			}
+			if (ch == '\"')
+			{
+				state = 33;
+			}
+			break;
+		case 17:
+			if (ch == '/')
+			{
+				pCrtCh++;
+				state = 20;
+			}
+			else if (ch == '*')
+			{
+				pCrtCh++;
+				state = 18;
+			}
+			else
+				state = 60;
+			break;
+		case 18:
+			if (ch != '*')
+			{
+				pCrtCh++;
+			}
+			else
+				state = 19;
+			break;
+		case 19:
+			if (ch == '*')
+			{
+				pCrtCh++;
+				comment = true;
+			}
+			else if (ch == '/' && comment)
+			{
+				pCrtCh++;
+				state = 34;
+				comment = false;
+			}
+			else
+				state = 18;
+			break;
+		case 20:
+			state = 21;
+			break;
+		case 21:
+			if (ch != ' ' && ch != '\n' && ch != '\r' && ch != '\0')
+				pCrtCh++;
+			else 
+				state = 61;
+			break;
+		case 22:
+			if (isalnum(ch) || ch == '_')
+				pCrtCh++;
+			else
+				state = 36;
+			break;
+		case 23:
+			if (ch == '=')
+			{
+				pCrtCh++;
+				state = 43;
+			}
+			else
+				state = 42;
+			break;
+		case 24:
+			if (ch == '=')
+			{
+				pCrtCh++;
+				state = 45;
+			}
+			else
+				state = 44;
+			break;
+		case 25:
+			if (ch == '=')
+			{
+				pCrtCh++;
+				state = 47;
+			}
+			else
+				state = 46;
+			break;
+		case 26:
+			if (ch == '=')
+			{
+				pCrtCh++;
+				state = 57;
+			}
+			else if (ch != '=')
+				state = 56;
+			else 
+				tkerr(addTk(END), "caracter invalid 26\n");
+			break;
+		case 27:
+			if (ch == '&')
+			{
+				pCrtCh++;
+				state = 53;
+			}
+			else
+				tkerr(addTk(END), "caracter invalid 27\n");
+			break;
+		case 28:
+			if (ch == '|')
+			{
+				pCrtCh++;
+				state = 54;
+			}
+			else
+				tkerr(addTk(END), "caracter invalid 28\n");
+			break;
+		case 29:
+			tk = addTk(CT_INT);
+			int_char = createString(pStartCh, pCrtCh);
+			if (hex)
+				sscanf(int_char, "%x", &tk->i);
+			else 
+				sscanf(int_char, "%d", &tk->i);
+			return CT_INT;
+		case 30:
+			tk = addTk(CT_REAL);
+			real = createString(pStartCh, pCrtCh);
+			tk->r = atof(real);
+			return CT_INT;
+		case 32:
+			tk = addTk(CT_CHAR);
+			tk->text = createString(pStartCh, pCrtCh);
+			return CT_CHAR;
+		case 33:
+			tk = addTk(CT_STRING);
+			tk->text = createString(pStartCh, pCrtCh);
+			return CT_STRING;
+		case 34:
+			tk = addTk(COMMENT);
+			tk->text = createString(pStartCh, pCrtCh);
+			return COMMENT;
+		case 35:
+			addTk(COMMA);
+			return COMMA;
+		case 36:
+				tk = addTk(ID);
+				tk->text = createString(pStartCh, pCrtCh);
+			return tk->code;
+		case 37:
+			addTk(SEMICOLON);
+			return SEMICOLON;
+		case 38:
+			addTk(LPAR);
+			return LPAR;
+		case 39:
+			addTk(RPAR);
+			return RPAR;
+		case 40:
+			addTk(LBRACKET);
+			return LBRACKET;
+		case 41:
+			addTk(RBRACKET);
+			return RBRACKET;
+		case 42:
+			addTk(NOT);
+			return NOT;
+		case 43:
+			addTk(NOTEQ);
+			return NOTEQ;
+		case 44:
+			addTk(LESS);
+			return LESS;
+		case 45:
+			addTk(LESSEQ);
+			return LESSEQ;
+		case 46:
+			addTk(GREATER);
+			return GREATER;
+		case 47:
+			addTk(GREATEREQ);
+			return GREATEREQ;
+		case 48:
+			addTk(LACC);
+			return LACC;
+		case 49:
+			addTk(RACC);
+			return RACC;
+		case 50:
+			addTk(ADD);
+			return ADD;
+		case 51:
+			addTk(SUB);
+			return SUB;
+		case 52:
+			addTk(MUL);
+			return MUL;
+		case 53:
+			addTk(AND);
+			return AND;
+		case 54:
+			addTk(OR);
+			return OR;
+		case 55:
+			addTk(DOT);
+			return DOT;
+		case 56:
+			addTk(ASSIGN);
+			return ASSIGN;
+		case 57:
+			addTk(EQUAL);
+			return EQUAL;
+		case 58:
+			addTk(SPACE);
+			return SPACE;
+		case 59:
+			addTk(END);
+			return END;
+		case 60:
+			addTk(DIV);
+			return DIV;
+		case 61:
+			tk = addTk(CT_CHAR);
+			tk->text = createString(pStartCh, pCrtCh);
+			return CT_CHAR;
 		}
 	}
 }
 
+void showAtoms()
+{
+	Token *tk;
+	int old_line = 1;
+	printf("1->");
+	for (tk = tokens; tk != NULL; tk = tk->next)
+	{
+		if (old_line != tk->line)
+		{
+			printf("\n\n");
+			printf("%d->", tk->line);
+			old_line = tk->line;
+		}
+		printf("%s (%d) ",names[tk->code], tk->code);
+			switch (tk->code)
+		{
+		case ID: printf(":\"%s\"  ", tk->text); break;
+		case CT_STRING: printf(": \"%s\"  ", tk->text);
+		case CT_CHAR: printf(": \"%s\"   ", tk->text); break;
+		case CT_INT: printf(": \"%d\"  ", tk->i); break;
+		case CT_REAL: printf(": \"%f\"  ", tk->r); break;
+		}
+			
+	}
+	printf("\n\n");
+}
 int main()
 {
 	FILE *fis;
 
-	if ((fis = fopen("pr.c", "r")) == NULL){
+	if ((fis = fopen("8.txt", "r")) == NULL){
 		perror("Fisier inexistent");
 		exit(1);
 	}
@@ -118,8 +714,15 @@ int main()
 	int nc = fread(input, sizeof(char), 50000, fis);
 
 	input[nc] = '\0';
+	pCrtCh = input;
+
+	do{
+		getNextToken();
+	} while (lastToken->code != END);
+
+	showAtoms();
+
 	fclose(fis);
 
 	return 0;
 }
-
---
+++ Compilator/states.h
@@ -0,0 +1,35 @@
+#define S_CT_INT		1
+#define S_CT_REAL		2
+#define S_CT_CHAR		3
+#define S_CT_STRING		4
+#define S_COMMENT		5
+
+#define S_ID			6	
+
+#define S_COMMA			7
+#define S_SEMICOLON		8
+#define S_LPAR			9
+#define S_RPAR			10
+#define S_LBRACKET		11
+#define S_RBRACKET		12
+#define S_LACC			13
+#define S_RACC			14
+
+#define S_ADD			15
+#define S_SUB			16
+#define S_MULL			17
+#define S_DIV			18
+#define S_DOT			19
+#define S_AND			20
+#define S_OR			21
+#define S_NOT			22
+#define S_ASSIGN		23
+#define S_EQUAL			24
+#define S_NOTEQ			25
+#define S_LESS			26
+#define S_LESSEQ		27
+#define S_GREATER		28
+#define S_GREATEREQ		29
+#define S_SPACE			30
+
+#define S_END			31
